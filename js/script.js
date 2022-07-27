2dd63eecaebbbdd6e35886628849971a604145ac
Elshodbek <elshod.tukhtamurodov@gmail.com>
2022-07-27 14:17:39 +0500

    v1.1.4

--- js/script.js
+++ js/script.js
@@ -1,8 +1,23 @@
 const btnMenu = document.querySelector(".header__menu-icon");
-if(btnMenu){
+if (btnMenu) {
    const menu = document.querySelector(".header__menu");
-   btnMenu.addEventListener("click",(e)=>{
+   btnMenu.addEventListener("click", (e) => {
       btnMenu.classList.toggle("_active");
       menu.classList.toggle("_open");
+   })
+}
+
+const openModalButton = document.querySelector("._open-modal");
+if (openModalButton) {
+   openModalButton.addEventListener("click", () => {
+      const modal = document.querySelector(".modal");
+      modal.classList.add("_active");
+      const clouseModalButton = document.querySelectorAll("._clouse-modal");
+      for (let index = 0; index < clouseModalButton.length; index++) {
+         const btnClouse = clouseModalButton[index];
+         btnClouse.addEventListener("click", () => {
+            modal.classList.remove("_active");
+         })
+      }
    })
 }
